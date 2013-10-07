a794101b57ae9e262f33baf2ce1ef9d620828034
Marcin Kubala <marcin@mkubala.pl>
2013-10-07 19:55:19 +0200

    Add more examples

--- src/main/java/pl/mkubala/TypesafeConfigJavaExamples.java
+++ src/main/java/pl/mkubala/TypesafeConfigJavaExamples.java
@@ -1,7 +1,11 @@
 package pl.mkubala;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static java.lang.System.out;
 
@@ -9,18 +13,122 @@ public class TypesafeConfigJavaExamples {
 
     private final Config config = ConfigFactory.load();
 
+    private static void display(final String code, final Object results) {
+        out.println(String.format("%s \n\t\t\t=> %s\n", code, results));
+    }
+
     public static void main(final String[] args) {
         TypesafeConfigJavaExamples examples = new TypesafeConfigJavaExamples();
 
         examples.simpleReading();
+        examples.extractNestedConfig();
+        examples.fallbackExample();
+        examples.listingPathsAndKeys();
+        examples.valuesOverriding();
+        examples.creatingAndRendering();
     }
 
     private void simpleReading() {
-        out.println(config.getString("my.organization.project.name"));
-        out.println(config.getString("my.organization.project.description"));
-        out.println(config.getInt("my.organization.team.avgAge"));
-        out.println(config.getStringList("my.organization.team.members"));
+        display("config.getString(\"my.organization.project.name\")",
+                config.getString("my.organization.project.name"));
+
+        // my.organization.project.description contains substitution
+        display("config.getString(\"my.organization.project.description\")",
+                config.getString("my.organization.project.description"));
+
+        display("config.getInt(\"my.organization.team.avgAge\")",
+                config.getInt("my.organization.team.avgAge"));
+
+        display("config.getStringList(\"my.organization.team.members\")",
+                config.getStringList("my.organization.team.members"));
     }
 
+    private void extractNestedConfig() {
+        Config extractedConfig = config.getConfig("my.organization.team");
+
+        display("extractedConfig.getInt(\"avgAge\")",
+                extractedConfig.getInt("avgAge"));
+
+        display("extractedConfig.getStringList(\"members\")",
+                extractedConfig.getStringList("members"));
+    }
+
+    private void fallbackExample() {
+        Config customs = ConfigFactory.load("custom").withFallback(config);
+
+        display("customs.getString(\"my.organization.project.name\")",
+                customs.getString("my.organization.project.name"));
+
+        display("customs.getString(\"my.organization.team.avgAge\")",
+                customs.getString("my.organization.team.avgAge"));
+    }
+
+    private void listingPathsAndKeys() {
+        // List paths
+        Set<String> keys = new HashSet<>();
+        for (Map.Entry<String, ConfigValue> entry : config.entrySet()) {
+            keys.add(entry.getKey());
+        }
+        display("config.entrySet() .. .getKey()",
+                keys);
+
+        // List root's direct children keys
+        display("config.root().keySet()",
+                config.root().keySet());
+
+        // List only my.organization sub-paths
+        Set<String> keys2 = new HashSet<>();
+        for (Map.Entry<String, ConfigValue> entry : config.withOnlyPath("my.organization").entrySet()) {
+            keys2.add(entry.getKey());
+        }
+        display("config.withOnlyPath(\"my.organization\").entrySet() .. .getKey()",
+                keys2);
+
+    }
+
+    private void valuesOverriding() {
+        // Displays original value
+        display("config.getString(\"my.organization.project.name\")",
+                config.getString("my.organization.project.name"));
+
+        // Config is immutable
+        Config updatedConfig = config.withValue("my.organization.project.name", ConfigValueFactory.fromAnyRef("RebrandedProject"));
+        display("updatedConfig = config.withValue(\"my.organization.project.name\", ConfigValueFactory.fromAnyRef(\"RebrandedProject\")",
+                "[OK]");
+
+        // Displays updated value
+        display("updatedConfig.getString(\"my.organization.project.name\")",
+                updatedConfig.getString("my.organization.project.name"));
+
+        // Currently I have no idea how to 'reload' substitutions so this one
+        // displays description with old project's name :(
+        display("updatedConfig.getString(\"my.organization.project.description\")",
+                updatedConfig.getString("my.organization.project.description"));
+
+        // Displays original value one more time - to prove the immutability
+        display("config.getString(\"my.organization.project.name\")",
+                config.getString("my.organization.project.name"));
+    }
+
+    private void creatingAndRendering() {
+        renderConfig(createDinnerConfig());
+    }
+
+    private Config createDinnerConfig() {
+        return ConfigFactory.empty()
+                .withValue("dish.name", ConfigValueFactory.fromAnyRef("SomeCompany"))
+                .withValue("dish.estimatedCost", ConfigValueFactory.fromAnyRef(10))
+                .withValue("dish.ingredients", ConfigValueFactory.fromIterable(Arrays.asList(
+                        "potato", "bacon", "onion", "salt", "pepper"
+                )));
+    }
+
+    private void renderConfig(final Config config) {
+        // See ConfigRenderOptions' JavaDoc for details.
+        ConfigRenderOptions renderOpts = ConfigRenderOptions.defaults().setOriginComments(false).setComments(false);
+        //ConfigRenderOptions renderOpts = ConfigRenderOptions.concise();
+
+        display("config.root().render(renderOpts)", config.root().render(renderOpts));
+    }
 
 }
--- src/main/scala/pl/mkubala/TypesafeConfigScalaExamples.scala
+++ src/main/scala/pl/mkubala/TypesafeConfigScalaExamples.scala
@@ -1,20 +1,126 @@
 package pl.mkubala
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config._
 
 object TypesafeConfigScalaExamples {
 
   val config = ConfigFactory.load()
 
-  def main(args: Array[String]) {
-    basicReading()
+  def display(codeAsString: String)(body: => Any) {
+    println(s"$codeAsString \n\t\t\t=> $body\n")
   }
 
-  def basicReading() {
-    println(config.getString("my.organization.project.name"))
-    println(config.getString("my.organization.project.description"))
-    println(config.getInt("my.organization.team.avgAge"))
-    println(config.getStringList("my.organization.team.members"))
+  def main(args: Array[String]) {
+    basicReading()
+    extractNestedConfig()
+    listingPathsAndKeys()
+    valuesOverriding()
+    creatingAndRendering()
+  }
+
+  private def basicReading() {
+    display("config.getString(\"my.organization.project.name\")") {
+      config.getString("my.organization.project.name")
+    }
+
+    // my.organization.project.description contains substitution
+    display("config.getString(\"my.organization.project.description\")") {
+      config.getString("my.organization.project.description")
+    }
+
+    display("config.getInt(\"my.organization.team.avgAge\")") {
+      config.getInt("my.organization.team.avgAge")
+    }
+
+    display("config.getStringList(\"my.organization.team.members\")") {
+      config.getStringList("my.organization.team.members")
+    }
+  }
+
+  private def extractNestedConfig() {
+    val extractedConfig: Config = config.getConfig("my.organization.team");
+
+    display("extractedConfig.getInt(\"avgAge\")") {
+      extractedConfig.getInt("avgAge")
+    }
+
+    display("extractedConfig.getStringList(\"members\")") {
+      extractedConfig.getStringList("members")
+    }
+  }
+
+  private def listingPathsAndKeys() {
+    import scala.collection.JavaConversions._
+
+    // List paths
+    display("config.entrySet() map (_.getKey)") {
+      config.entrySet() map (_.getKey)
+    }
+
+    // List root's direct children keys
+    display("config.root().keySet()") {
+      config.root().keySet()
+    }
+
+    // List only my.organization sub-paths
+    display("config.withOnlyPath(\"my.organization\").entrySet() map (_.getKey)") {
+      config.withOnlyPath("my.organization").entrySet() map (_.getKey)
+    }
+
+  }
+
+  private def valuesOverriding() {
+    // Displays original value
+    display("config.getString(\"my.organization.project.name\")") {
+      config.getString("my.organization.project.name")
+    }
+
+    // Config is immutable
+    val updatedConfig: Config = config.withValue("my.organization.project.name", ConfigValueFactory.fromAnyRef("RebrandedProject"))
+    display("updatedConfig = config.withValue(\"my.organization.project.name\", ConfigValueFactory.fromAnyRef(\"RebrandedProject\")") {
+      "[OK]"
+    }
+
+    // Displays updated value
+    display("updatedConfig.getString(\"my.organization.project.name\")") {
+      updatedConfig.getString("my.organization.project.name")
+    }
+
+    // Currently I have no idea how to 'reload' substitutions so this one
+    // displays description with old project's name :(
+    display("updatedConfig.getString(\"my.organization.project.description\")") {
+      updatedConfig.getString("my.organization.project.description")
+    }
+
+    // Displays original value one more time - to prove the immutability
+    display("config.getString(\"my.organization.project.name\")") {
+      config.getString("my.organization.project.name")
+    }
+  }
+
+  private def creatingAndRendering() {
+    renderConfig(createDinnerConfig())
+  }
+
+  private def createDinnerConfig(): Config = {
+    import scala.collection.JavaConversions._
+    ConfigFactory.empty()
+      .withValue("dish.name", ConfigValueFactory.fromAnyRef("SomeCompany"))
+      .withValue("dish.estimatedCost", ConfigValueFactory.fromAnyRef(10))
+      .withValue("dish.ingredients", ConfigValueFactory.fromIterable(
+      List(
+        "potato", "bacon", "onion", "salt", "pepper"
+      )))
+  }
+
+  private def renderConfig(config: Config) {
+    // See ConfigRenderOptions' JavaDoc for details.
+    val renderOpts = ConfigRenderOptions.defaults().setOriginComments(false).setComments(false);
+    //val renderOpts = ConfigRenderOptions.concise();
+
+    display("config.root().render(renderOpts)") {
+      config.root().render(renderOpts)
+    }
   }
 
 }
